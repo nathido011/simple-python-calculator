477a463e77594a2c48b074c1b4e93342272157f7
nathido011 <168593753+nathido011@users.noreply.github.com>
2024-05-07 18:21:01 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,8 @@
 def soma(a, b):
     resurt a + b 
-
-if__name__== "__main__"
+at
+if __name__== "__main__":
+  nu1 = float(input("Digite o primeiro número: "))
+  nu2 = float(input("Digite o segundo  número: "))
+  resultado = soma(nu1, nu2)
+  print(f"Resultado:{resultado}")
